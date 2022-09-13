48c87cace4c2c8de391bea0b9ff2ed352a3eba20
Colin Bankart <62899274+Ironwally@users.noreply.github.com>
2022-09-13 13:26:24 +0200

    Aufgabe 2_6 Vers1

--- Aufgabe_2_6.java
+++ Aufgabe_2_6.java
@@ -1,1 +1,10 @@
-
+public class Main {
+    public static void main(String[] args) {
+        boolean male = true;
+        String name = "Peter";
+        String ansprache;
+        if (male) {ansprache = "Herr"}
+        else ansprache = "Frau";
+        System.out.println("Guten Tag " + ansprache + name);
+    }
+}
