fff0e90988b546d561d309d02b04a91f739425b5
Ironwally <notmyname.eisern@gmail.com>
2022-09-14 13:50:08 +0200

    3.6 gelöst

--- Tag3/aufgaben/aufgabe_3_6.java
+++ Tag3/aufgaben/aufgabe_3_6.java
@@ -3,15 +3,23 @@ package Tag3.aufgaben;
 public class aufgabe_3_6 {
     public static void main(String[] args) {
 
-        int outprint = 0;
-        int i = 1;
-        while (outprint<1000) {
-            outprint = i;
-            System.out.println(outprint);
-            i = i+outprint;
+        int temp = 0;
+        int i = 0;
+        int y = 1;
+        while (y<1000) {
+            y += temp;
+            System.out.println(y);
+            temp += y;
+            System.out.println(temp);
+        }
 
 
-
+        int[] v = {1,0};
+        while (v[i%2]<1000) {
+            System.out.println(v[i%2]);
+            v[i%2] += v[(i+1)%2];
+            i++;
         }
     }
+
 }
