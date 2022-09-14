5c4fcad71ff039a140c71304b4ca10a914eecba3
Ironwally <notmyname.eisern@gmail.com>
2022-09-14 14:25:14 +0200

    Out of Memory Error hinzugefügt

--- Tag3/aufgaben/aufgabe_3_5.java
+++ Tag3/aufgaben/aufgabe_3_5.java
@@ -1,21 +1,33 @@
 package Tag3.aufgaben;
 
+import java.util.ArrayList;
+
 public class aufgabe_3_5 {
     public static void main(String[] args) {
-        int[] zahlenfolge = new int[] {4,8,15,16,23,42};
-        int[] newZahlen = {108,540,7200};
-        int[] beidesZusammen = new int[zahlenfolge.length+newZahlen.length];
-        for (int i=0; i<zahlenfolge.length;i++) {
+        int[] zahlenfolge = new int[]{4, 8, 15, 16, 23, 42};
+        int[] newZahlen = {108, 540, 7200};
+        int[] beidesZusammen = new int[zahlenfolge.length + newZahlen.length];
+        //Fügt Zahlen von zahlenfolge hinzu
+        for (int i = 0; i < zahlenfolge.length; i++) {
             beidesZusammen[i] = zahlenfolge[i];
         }
-        for (int i=0; i<newZahlen.length;i++) {
-            beidesZusammen[i+zahlenfolge.length] = newZahlen[i];
+        for (int i = 0; i < newZahlen.length; i++) {
+            beidesZusammen[i + zahlenfolge.length] = newZahlen[i];
         }
-        System.out.println();
-        for (int i=0;i< beidesZusammen.length;i++) {    //Ausgabe der Zahlen
+        //Ausgabe der Zahlen
+        for (int i = 0; i < beidesZusammen.length; i++) {
             System.out.println(beidesZusammen[i]);
         }
 
+        ArrayList arrayList = new ArrayList<Integer>();
+        arrayList.add(1);
+        while (true) {
+            try {
+                arrayList.addAll(arrayList);
+            } catch (OutOfMemoryError e) {
+                throw e;
+            }
+            System.out.println(arrayList.size());
+        }
     }
-
 }
